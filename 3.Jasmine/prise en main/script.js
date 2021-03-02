787f7106150a2456f578edeaf38f614db4449350
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2021-03-02 11:10:28 +0100

    Update script.js

--- 3.Jasmine/prise en main/script.js
+++ 3.Jasmine/prise en main/script.js
@@ -1,3 +1,34 @@
+describe('Prime numbers under 100 array', function(){
+  var primes = eratosthenes(100);
+
+  it('is truthy', function(){
+    expect(primes).toBeTruthy();
+  });
+
+  it('is really an array', function(){
+    expect(Array.isArray(primes)).toBe(true);
+  });
+
+  it('has 25 primes under 100', function(){
+    expect(primes.length).toEqual(25);
+  });
+
+  it('contains 5, 11, 43, 97 as primes', function(){
+    expect(primes).toContain(5);
+    expect(primes).toContain(11);
+    expect(primes).toContain(43);
+    expect(primes).toContain(97);
+  });
+
+  it('does not contain 28, 60, 99', function(){
+    expect(primes).not.toContain(28);
+    expect(primes).not.toContain(60);
+    expect(primes).not.toContain(99);
+  });
+});
+
+
+
 function eratosthenes(n) {
     var detectprimes = new Array(n),
         primes = new Array();
